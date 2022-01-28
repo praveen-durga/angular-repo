a0704b93dbb20d6ec6af28085a30e4186c099757
praveen-durga <praveen.durga@gmail.com>
2022-01-28 21:03:52 +0530

    1 file modified, 1 deleted

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
-import { HelloComponent } from './hello.component';
 import { ParentComponent } from './parent/parent.component';
 import { ChildComponent } from './child/child.component';
 import { CommonService } from './common.service';
 
 @NgModule({
   imports: [BrowserModule, FormsModule],
-  declarations: [AppComponent, HelloComponent, ParentComponent, ChildComponent],
+  declarations: [AppComponent, ParentComponent, ChildComponent],
   bootstrap: [AppComponent],
   providers: [CommonService],
 })
